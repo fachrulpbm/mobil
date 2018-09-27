2a9feae4edc3fd3c5a2625e1d8b00497dc2c13ea
fachrulpbm <fachrul.pbm@polindra.ac.id>
2018-09-27 08:57:04 +0700

    contoh 1

---
+++ Mobil.java
@@ -0,0 +1,27 @@
+public class Mobil{
+
+	int ban;	
+	int kecepatan;
+	int bensin;
+	String merk;
+
+	void maju(){
+		System.out.println("Mobil majuuu...");
+	}
+
+	void mundur(){
+		System.out.println("Mobil munduur...");
+	}
+
+	String showMerk(){
+		return merk;
+	}
+
+	// void addBensin(){
+
+	// }
+
+	// int showBensin(){
+		
+	// }
+}
